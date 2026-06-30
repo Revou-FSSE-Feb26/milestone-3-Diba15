e754ae420f1e7db0fad1474bc87dc423c0394444
Dimas Bagas Saputro <dimazzbagazz@yahoo.co.id>
2026-06-30 12:45:10 +0700

    refactor: Add image load error fallback in CartCard

--- src/components/cart/CartCard.tsx
+++ src/components/cart/CartCard.tsx
@@ -2,26 +2,34 @@ import { CartItem } from "@/types/Types";
 import Image from "next/image";
 
 export default function CartCard({ item, handleAdd, handleDecrease }: { item: CartItem, handleAdd: (item: CartItem) => void, handleDecrease: (item: CartItem) => void }) {
-    const { images, title, price, quantity } = item
+  const { images, title, price, quantity } = item
 
-    const itemPrice = typeof price === "number" ? price?.toFixed(2) : "N/A";
+  const itemPrice = typeof price === "number" ? price?.toFixed(2) : "N/A";
 
-    return (
-        <div className={"flex items-center gap-4 p-4 border-b border-gray-200"}>
-            <Image src={images[0]} alt={title} width={64} height={64} className={"object-cover rounded"} />
-            <div className={"flex-1"}>
-                <h2 className="font-extrabold tracking-tight">{title}</h2>
-                <p className="text-sm text-gray-500">Price: ${itemPrice}</p>
-            </div>
-            <div className={"flex flex-row gap-2 items-center"}>
-                <button onClick={() => handleDecrease(item)} className={"bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"}>
-                    -
-                </button>
-                <span>{quantity}</span>
-                <button onClick={() => handleAdd(item)} className={"bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"}>
-                    +
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div className={"flex items-center gap-4 p-4 border-b border-gray-200"}>
+      <div>
+        <Image src={images[0]} alt={title} width={64} height={64} className={"object-cover rounded"}
+          onError={(e) => {
+            e.currentTarget.style.display = 'none';
+            if (e.currentTarget.parentElement) {
+              e.currentTarget.parentElement.innerHTML = '<i class="fa-solid fa-image text-gray-400 text-2xl"></i>';
+            }
+          }} />
+      </div>
+      <div className={"flex-1"}>
+        <h2 className="font-extrabold tracking-tight">{title}</h2>
+        <p className="text-sm text-gray-500">Price: ${itemPrice}</p>
+      </div>
+      <div className={"flex flex-row gap-2 items-center"}>
+        <button onClick={() => handleDecrease(item)} className={"bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"}>
+          -
+        </button>
+        <span>{quantity}</span>
+        <button onClick={() => handleAdd(item)} className={"bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"}>
+          +
+        </button>
+      </div>
+    </div>
+  );
 }
